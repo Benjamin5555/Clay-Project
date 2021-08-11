885c8061f5d29702c7b2da07b327e710e531b48b
Benjamin Carpenter <s1731178@ed.ac.uk>
2021-08-11 20:46:12 +0100

    Made able to run rdf script from command line

--- analysis/rdf_atoms_ions.py
+++ analysis/rdf_atoms_ions.py
@@ -17,6 +17,8 @@ from MDAnalysis.topology import TPRParser
 from definitions import *
 import sys
 sys.path.append("../../../")
+sys.path.append("ClayAnalysis")
+
 from clayAnalysis import ClayAnalysis 
 
 
@@ -29,38 +31,23 @@ import sys
 
 tpr = sys.argv[1] 
 trr = sys.argv[2]
+sys = sys.argv[3]
 
 u=mda.Universe(tpr, trr,tpr_resid_from_one=False)
 
 cal = ClayAnalysis(u)
-
+	
 surfaces = cal.generate_surface_group("waters")
 w_surf_grp = cal.combine_atomgroups(surfaces[0])+cal.combine_atomgroups(surfaces[1])
 print(np.unique(w_surf_grp.types))
 w_surf_grp= w_surf_grp.select_atoms("type o*")
 ATs = u.atoms.select_atoms("type at*")
 STs = u.atoms.select_atoms("type st*")
-print(w_surf_grp)
-print(ATs)
-print(STs)
-
+	
 for ions in ["resname Cs", "resname K", "resname Na"]:
     ion_grp = u.atoms.select_atoms(ions)
-    print(np.unique(u.atoms.types))
     for surf_atm in [w_surf_grp,ATs,STs]: 
         average_rdf(ion_grp, surf_atm)
     
-    plt.savefig(str(ions)+".png") 
-    plt.show()
+    plt.savefig(str(sys)+"_"+str(ions)+".png") 
 
-#for ions in ["resname Cs", "resname K", "resname Na"]:
-#    ion_grp = u.atoms.select_atoms(ions)
-#    print(np.unique(u.atoms.types))
-#    surf_grp = u.atoms.select_atoms("resname NON*")
-#    print(ion_grp.n_atoms)
-#    average_rdf(ion_grp, surf_grp)
-#    plt.show()
-#
-#clay=mda.AtomGroup(u.select_atoms('resname NON*'))
-#atomgroup_coords(clay)
-#position_density(tpr, trr, 'resname NON*', False)
